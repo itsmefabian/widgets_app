7c5fe6aeac87d666f864af274e698b214bcdda8f
freyes <fabianhr.fr@gmail.com>
2026-08-26 03:46:01 -0500

    feat: enhance SnackbarScreen with dialog functionality and update layout

--- lib/config/presentation/screen/snackbar/snackbar_screen.dart
+++ lib/config/presentation/screen/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -17,10 +18,48 @@ class SnackbarScreen extends StatelessWidget {
     );
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Sure'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Close'),
+          ),
+          FilledButton(onPressed: () => context.pop(), child: const Text('Ok')),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Snackbar and dialogs')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                return showAboutDialog(
+                  context: context,
+                  children: [const Text('Hello world')],
+                );
+              },
+              child: Text('Expired license'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: Text('Show dialog'),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text('Show snackbar'),
         icon: Icon(Icons.remove_red_eye_outlined),
